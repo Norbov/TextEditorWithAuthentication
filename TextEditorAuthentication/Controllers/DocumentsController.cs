454aac6ce85e578a397e464d06cb33d7e8ce1287
Norbov <92054638+Norbov@users.noreply.github.com>
2024-03-31 21:42:48 +0200

    Add project files.

---
+++ TextEditorAuthentication/Controllers/DocumentsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TextEditorAuthentication.Data;
+using TextEditorAuthentication.Models;
+
+namespace TextEditorAuthentication.Controllers
+{
+    [Authorize]
+    public class DocumentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocumentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Documents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = from d in _context.Documents select d;
+            applicationDbContext = applicationDbContext.Where(x => x.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            return View(await applicationDbContext.Include(d => d.User).ToListAsync());
+        }
+
+        // GET: Documents/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Documents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Content,UserId")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(document);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(document);
+        }
+
+        // GET: Documents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents.FindAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+            if (document.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // POST: Documents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Content,UserId")] Document document)
+        {
+            if (id != document.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(document);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocumentExists(document.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", document.UserId);
+            return View(document);
+        }
+
+        // GET: Documents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var document = await _context.Documents
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+            if (document.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+
+            return View(document);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var document = await _context.Documents.FindAsync(id);
+            if (document != null)
+            {
+                _context.Documents.Remove(document);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocumentExists(int id)
+        {
+            return _context.Documents.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TextEditorAuthentication/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TextEditorAuthentication.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        [Required]
+        public string? UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+    }
+}
